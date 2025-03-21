21874eacdf1795cb8baa5f3dcd2e870775ed7c05
Oceana Gershberg <75436638+oceana1129@users.noreply.github.com>
2025-03-21 14:40:04 -0700

    refined damage script with more error handling and an on destroy effects

--- Assets/_FinalProject/Scripts/DamageZone.cs
+++ Assets/_FinalProject/Scripts/DamageZone.cs
@@ -4,11 +4,46 @@ using System.Collections;
 
 public class DamageZone : MonoBehaviour
 {
+    [Header("DamageSettings")]
     public bool instantKill = false;        // toggle for instant death 
     public int damageAmount = 1;            // damage taken when hitting damage zone
     public float damageCooldown = 1.0f;    // cooldown time in seconds
+
+    [Header("Destroy Object Settings")]
+    public bool destroyOnContact = false;
+    public float destroyTimer = 1f;
+
+    // private variables
     private bool canDamage = true;          // used for cooldowns
 
+
+    private void Start()
+    {
+        // gameobject must have at least one collider available
+        Collider collider = GetComponent<Collider>();
+
+        if (collider == null)
+        {
+            Debug.LogError($"[DamageZone] ERROR: No collider found on '{gameObject.name}'. Must attach collider with 'IsTrigger' enabled.");
+            enabled = false; // disable script
+            return;
+        }
+
+        // see if collider is disabled in inspector
+        if (!collider.enabled)
+        {
+            Debug.LogError($"[DamageZone] ERROR: Collider on '{gameObject.name}' is DISABLED. Please enable it.");
+            enabled = false; // disable script
+            return;
+        }
+
+        if (!collider.isTrigger)
+        {
+            Debug.LogError($"[DamageZone] ERROR: Collider on '{gameObject.name}' does not have 'Is Trigger' enabled. Please enable it.");
+            enabled = false;    // disable script
+            return;
+        }
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -37,6 +72,9 @@ public class DamageZone : MonoBehaviour
             playerHealth.TakeDamage(damage);            // player takes damage
             StartCoroutine(DamageCooldownRoutine());    // apply cooldown as needed
         }
+
+        if (destroyOnContact)
+            Destroy(gameObject, destroyTimer);
     }
 
     private IEnumerator DamageCooldownRoutine()
