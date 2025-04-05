ef2d43aec6b22930deba837de601d0ac80421d6e
ZC0211 <zuoyinchen1014@gmail.com>
2025-04-05 00:35:02 -0700

    Update LevelManager

--- Assets/_FinalProject/Scripts/LevelManager.cs
+++ Assets/_FinalProject/Scripts/LevelManager.cs
@@ -4,33 +4,55 @@ using System.Collections.Generic;
 public class LevelManager : MonoBehaviour
 {
     // tracks the number of objectives to be completed to unlock the door
-    // requiredPadsToRead should be set to the number of pads
-    // in the scene in the inspection
+    // requiredPadsToRead should be set to the number of pads in the scene
+    public bool isMiloObjective = false;
+    public bool isReadingLogsObjective = false;
     public int requiredPadsToRead = 0;
+    private bool isMiloActivated = false;
+    private bool isReadingLogsCompleted = false;
     private int padsRead;
     private HashSet<Transform> readPads;
 
     // if requiredPadsToRead is 0, the door will be unlocked immediately 
     void Start()
     {
-        padsRead = 0;
-        readPads = new HashSet<Transform>();
-
-        if (requiredPadsToRead == 0)
+        if (isReadingLogsObjective)
         {
-            EnableDoor();
+            padsRead = 0;
+            readPads = new HashSet<Transform>();
+        }
+    }
+
+    private void Update()
+    {
+        if (isReadingLogsObjective && isMiloObjective)
+        {
+            if (isMiloActivated && isReadingLogsCompleted)
+            {
+                EnableDoor();
+            }
+        }
+
+        if (isMiloObjective && !isReadingLogsObjective)
+        {
+            if (isMiloActivated)
+            {
+                EnableDoor();
+            }
+        }
+        else if (isReadingLogsObjective && !isMiloObjective)
+        {
+            if (isReadingLogsCompleted)
+            {
+                EnableDoor();
+            }
         }
     }
 
     // the method to call when a pad is read and open the door if all pads are read
     public void PadRead(Transform pad)
     {
-        if (pad == null)
-        {
-            return;
-        }
-
-        if (readPads == null)
+        if (pad == null || readPads == null || isReadingLogsObjective == false)
         {
             return;
         }
@@ -42,7 +64,7 @@ public class LevelManager : MonoBehaviour
 
             if (padsRead >= requiredPadsToRead)
             {
-                EnableDoor();
+                isReadingLogsCompleted = true;
             }
         }
     }
@@ -58,6 +80,10 @@ public class LevelManager : MonoBehaviour
             {
                 doorBehavior = door.GetComponentInChildren<DoorBehavior>();
             }
+            if (doorBehavior == null)
+            {
+                doorBehavior = door.GetComponentInParent<DoorBehavior>();
+            }
 
             if (doorBehavior != null)
             {
@@ -65,6 +91,11 @@ public class LevelManager : MonoBehaviour
                 return;
             }
         }
+    }
+
+    public void MiloActivated()
+    {
+        isMiloActivated = true;
     }
 }
 
