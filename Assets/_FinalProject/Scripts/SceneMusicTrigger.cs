0f522f5b53ba524bf814018d9b6615bb6a378d16
Oceana Gershberg <75436638+oceana1129@users.noreply.github.com>
2025-04-19 15:55:40 -0700

    fixed changing scene audio

--- Assets/_FinalProject/Scripts/SceneMusicTrigger.cs
+++ Assets/_FinalProject/Scripts/SceneMusicTrigger.cs
@@ -9,6 +9,11 @@ public class SceneMusicTrigger : MonoBehaviour
         if (sceneClip != null && AudioManager.instance != null)
         {
             AudioManager.instance.ChangeTrack(sceneClip);
+            Debug.Log("changed scene audio");
         }
+        else {
+            Debug.Log("Did not change scene audio");
+        }
+        
     }
 }
